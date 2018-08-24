d716f6ed693505d83dc79073a0ad6c1b75463e4b
Meria Thomas <josemeria93@gmail.com>
2018-08-24 11:02:39 -0700

    add front-end logic

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,19 @@ var toppingName = {cheese: 3, pepperoni:5 , artichoke: 4 };
 var sizeName = {small:3, medium: 4, large: 5};
 
 Pizza.prototype.amount = function(){
-  return 
-}
+  return toppingName[this.topping] + sizeName[this.size];
+};
 
 
 $(document).ready(function(){
   $("#input").submit(function(event){
     event.preventDefault();
+    var topping = $("input:radio[name=topping]:checked").val();
+    var size = $("input:radio[name=size]:checked").val();
+    console.log(topping);
+    var newTotal = new Pizza(topping,size);
+
+    $("#result").append("<span class=result>" + newTotal.amount() +"</span>");
+
   })
 });
